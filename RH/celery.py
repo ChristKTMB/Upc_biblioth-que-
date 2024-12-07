03dae24530c2c69cfc805ef4077b9c553146e9fb
Christ KATUMBA <christkatumba02@gmail.com>
2024-12-07 14:54:59 +0100

    Ajout de l'ingress

--- RH/celery.py
+++ RH/celery.py
@@ -3,14 +3,10 @@ import os
 from celery import Celery
 from django.conf import settings
 
-# Configure les variables d'environnement pour Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RH.settings')
 
-# Créez une instance de Celery et configurez-la
 app = Celery('RH')
 
-# Charge la configuration à partir des paramètres Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Recherche automatiquement les tâches dans toutes les applications installées
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
